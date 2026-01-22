0cd1db49fa6ca90d1679fa00836ffbf20b79ac69
Sergey Senin <04sersen@gmail.com>
2026-01-22 16:40:44 +0300

    Выполнение задачи 46

---
+++ src/main/java/org/study/bootcamp/task_46/witcher_magic_map/v1/domain/model/Monster.java
@@ -0,0 +1,27 @@
+package org.study.bootcamp.task_46.witcher_magic_map.v1.domain.model;
+
+public class Monster {
+
+    private final String name;
+    private final Location location;
+
+    public Monster(String name, Location location) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Название монстра не должно быть пустым");
+        }
+        if (location == null) {
+            throw new IllegalArgumentException("Местоположение монстра не должно быть null");
+        }
+
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
